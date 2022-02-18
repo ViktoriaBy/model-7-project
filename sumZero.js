40a2397085b2a730bab1aa7a69126bf5ffcca14e
ViktoriaB <v.chamborant@hotmail.com>
2022-02-18 09:50:00 -0600

    Fixed answer to part 1

---
+++ sumZero.js
@@ -0,0 +1,21 @@
+//Write a function that takes in an array of numbers. 
+//The function should return True if any two numberss in list sum to 0, and false otherwise.
+
+
+
+
+
+
+
+
+addToZero([]);
+// -> False
+
+addToZero([1]);
+// -> False
+
+addToZero([1, 2, 3]);
+// -> False
+
+addToZero([1, 2, 3, -2]);
+// -> True
